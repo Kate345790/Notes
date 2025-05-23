dcc961e4c7f3c2684ce11f92b3f99e3d58c7f19a
Kate345790 <dubrovina.ekaterina291107@gmail.com>
2025-05-23 22:05:31 +0300

    просто

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -27,48 +27,40 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab_add;
     NotesListAdapter notesListAdapter;
     //Объявление БД
-    RoomDB database;
-    List<Notes> notes = new ArrayList<>();
+    // RoomDB database;
+    //List<Notes> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Присвоение элементов на макете
-        recyclerView = findViewById(R.id.recycler_home);
-        recyclerView = findViewById(R.id.fab_add);
-        database = RoomDB.getInstance(this);
-        notes = database.mainDAO().getAll();
+        //database = RoomDB.getInstance(this);
+        //notes = database.mainDAO().getAll();
 
-        updateRecycler(notes);
-
-        fab_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        //updateRecycler(notes);
 
             }
-        });
-
-
-
-        }
 
     private void updateRecycler(List<Notes> notes) {
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
-        notesListAdapter = new NotesListAdapter(MainActivity.this, notes,notesClickListener );
-        recyclerView.setAdapter(notesListAdapter);
     }
-    private final NotesClickListener notesClickListener = new NotesClickListener() {
-        @Override
-        public void onClick(Notes notes) {
-            Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
-            startActivityForResult(intent, 101);
-        }
 
-        @Override
-        public void onLongClick(Notes notes, CardView cardView) {
 
-        }
-    };
 }
+
+//private void updateRecycler(List<Notes> notes) {
+// recyclerView.setHasFixedSize(true);
+// recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
+// notesListAdapter = new NotesListAdapter(MainActivity.this, notes,notesClickListener );
+// recyclerView.setAdapter(notesListAdapter);
+// }
+// private final NotesClickListener notesClickListener = new NotesClickListener() {
+// @Override
+// public void onClick(Notes notes) {
+// Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
+// startActivityForResult(intent, 101);
+// }
+// @Override
+// public void onLongClick(Notes notes, CardView cardView) {
+// }
+// };
